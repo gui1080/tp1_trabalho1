d8923189418f9e541d7f84529b8a0bd422b75d50
Guilherme Braga <gbp1999@gmail.com>
2019-04-04 18:53:25 -0300

    Add files via upload

--- Trabalho1_TP1/dominios.h
+++ Trabalho1_TP1/dominios.h
@@ -10,21 +10,93 @@ class Codigo_de_Evento {
 private:
         int codigo_de_evento;
 
+        // temos que chear se o numero tem 3 digitos apenas
+
         const static int LIMITE = 999;
 
         // Método responsável por validação.
 
-        bool validar(int);
+        void validar(int) throw (invalid_argument);
 
 public:
         // Métodos de acesso.
 
-        void setCodigo_de_Evento(int);
+        void setCodigo_de_Evento(int) throw (invalid_argument);
 
         int getCodigo_de_Evento() const {
             return codigo_de_evento;
         }
 };
+
+class Codigo_de_Apresentacao {
+private:
+        int codigo_de_apresentacao;
+
+        // temos que chear se o numero tem 4 digitos apenas
+
+        const static int LIMITE = 9999;
+
+        // Método responsável por validação.
+
+        void validar(int) throw (invalid_argument);
+
+public:
+        // Métodos de acesso.
+
+        void setCodigo_de_Apresentacao(int) throw (invalid_argument);
+
+        int getCodigo_de_Apresentacao() const {
+            return codigo_de_apresentacao;
+        }
+};
+
+class Numero_de_Sala {
+private:
+        int numero_de_sala;
+
+        // temos que chear se o numero e maior que zero e menor ou igual a 10
+
+        const static int LIMITE_INFERIOR = 1;
+        const static int LIMITE_SUPERIOR = 10;
+
+        // Método responsável por validação.
+
+        void validar(int) throw (invalid_argument);
+
+public:
+        // Métodos de acesso.
+
+        void setNumero_de_Sala (int) throw (invalid_argument);
+
+        int getNumero_de_Sala () const {
+            return numero_de_sala;
+        }
+};
+
+class Preco {
+private:
+        float preco;
+
+        // temos que chear se o numero e maior ou igual que zero e menor ou igual a mil
+        // incluindo zero porque suponho que alguem va registrar um evento gratis neste sistema
+
+        const static int LIMITE_INFERIOR = 0;
+        const static int LIMITE_SUPERIOR = 1000;
+
+        // Método responsável por validação.
+
+        void validar(float) throw (invalid_argument);
+
+public:
+        // Métodos de acesso.
+
+        void setPreco (float) throw (invalid_argument);
+
+        float getPreco () const {
+            return preco;
+        }
+};
+
 
 
 
--- Trabalho1_TP1/main.cpp
+++ Trabalho1_TP1/main.cpp
@@ -5,26 +5,75 @@ using namespace std;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-void Codigo_de_Evento::setCodigo_de_Evento(int novo_codigo_de_evento) {
+void Codigo_de_Evento::setCodigo_de_Evento(int novo_codigo_de_evento) throw (invalid_argument){
 
-    if(validar(novo_codigo_de_evento) == 0){
-        codigo_de_evento = novo_codigo_de_evento;
-    }
-    else{
-        cout << "deu ruim meu patrão";
-    }
+    validar(novo_codigo_de_evento);
+    codigo_de_evento = novo_codigo_de_evento;
+
 }
+void Codigo_de_Evento::validar(int novo_codigo_de_evento) throw (invalid_argument){
 
-bool Codigo_de_Evento::validar(int novo_codigo_de_evento){
-
-    if( novo_codigo_de_evento > LIMITE){
-        return 1;
-    }
-    else if (novo_codigo_de_evento < LIMITE){
-        return 0;
+    if(novo_codigo_de_evento > LIMITE){
+        throw invalid_argument ("deu ruim meu consagrado");
     }
 
 }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void Codigo_de_Apresentacao::setCodigo_de_Apresentacao(int novo_codigo_de_apresentacao) throw (invalid_argument){
+
+    validar(novo_codigo_de_apresentacao);
+    codigo_de_apresentacao = novo_codigo_de_apresentacao;
+
+}
+void Codigo_de_Apresentacao::validar(int novo_codigo_de_apresentacao) throw (invalid_argument){
+
+    if(novo_codigo_de_apresentacao > LIMITE){
+        throw invalid_argument ("deu ruim");
+    }
+
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void Numero_de_Sala::setNumero_de_Sala(int novo_numero_de_sala) throw (invalid_argument){
+
+    validar(novo_numero_de_sala);
+    numero_de_sala = novo_numero_de_sala;
+
+}
+void Numero_de_Sala::validar(int novo_numero_de_sala) throw (invalid_argument){
+
+    if(novo_numero_de_sala >= LIMITE_SUPERIOR){
+        throw invalid_argument ("deu ruim");
+    }
+    else if(novo_numero_de_sala <= LIMITE_INFERIOR){
+        throw invalid_argument ("deu ruim");
+    }
+
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void Preco::setPreco(float novo_preco) throw (invalid_argument){
+
+    validar(novo_preco);
+    preco = novo_preco;
+
+}
+void Preco::validar(float novo_preco) throw (invalid_argument){
+
+    if(novo_preco > LIMITE_SUPERIOR){
+        throw invalid_argument ("deu ruim");
+    }
+    else if(novo_preco < LIMITE_INFERIOR){
+        throw invalid_argument ("deu ruim");
+    }
+
+}
+
+
 
 
 ///////////não sei pq mas não funfou botar essa função no arquivo de dominios.cpp (o certo), tenho que arrumar isso mais tarde/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -33,13 +82,42 @@ int main()
 {
 
     //daqui rodaremos os testes
-    cout << "to com saudade do printf\n";
+    cout << "to com saudade do printf e com saudade do terminal de Linux\n";
 
+    /*  Teste que fiz pra saber se a classe de evento funcionava
     int x;
     Codigo_de_Evento codigo1;
     codigo1.setCodigo_de_Evento(123);
     x = codigo1.getCodigo_de_Evento();
     cout << x;
+    */
+
+    /*  Teste que fiz pra saber se a classe de apresentacoes funcionava
+    int x;
+    Codigo_de_Apresentacao codigo1;
+    codigo1.setCodigo_de_Apresentacao(1234);
+    x = codigo1.getCodigo_de_Apresentacao();
+    cout << x;
+    */
+
+    /*  Teste que fiz pra saber se a classe de numero de sala funcionava
+    int x;
+    Numero_de_Sala num1;
+    num1.setNumero_de_Sala(3);
+    x = num1.getNumero_de_Sala();
+    cout << x;
+    */
+
+    /*  Teste que fiz pra saber se a classe de preco funcionava
+    float x;
+    Preco quantia1;
+    quantia1.setPreco(450.50);
+    x = quantia1.getPreco();
+    cout << x;
+    */
+
+
+
 
     return 0;
 }
